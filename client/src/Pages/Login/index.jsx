27467c3466289baf0da3af8089067ac9859654d1
nicolas <nicolasgpla@gmail.com>
2024-07-03 11:39:30 -0500

    M - login page layout

--- client/src/Pages/Login/index.jsx
+++ client/src/Pages/Login/index.jsx
@@ -6,7 +6,21 @@ function Login() {
     return(
         <>
             <Layout>
-                Login
+                <div className="login-container">
+                    <h1 className="login-container__h1">PATRON</h1>
+                    <div className="login-container-div">
+                        <span className="login-container-div__span">Welcome back</span>
+                        <p className="login-container-div__p">Please sign into your account</p>
+                    </div>
+                    <form action="" className="login-container-form">
+                        <input className="login-container-form__input--email" type='text' placeholder="Email" />
+                        <input className="login-container-form__input--password" type='password' placeholder="Password"/>
+                        <p className="login-container-form__p">Forgot Password?</p>
+                        <input className="login-container-form__input--button" type='button' value='Log in'/>
+                    </form>
+                    <p className="login-container__p">Don’t have an account? <span className="login-container__p__span">Create acount</span>
+                    </p>
+                </div>
             </Layout>
         </>
     )
